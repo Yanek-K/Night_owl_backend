f2002655bfe005d781fb7cf51cafa8249a37c667
Yanek-K <yanek.dev@gmail.com>
2023-02-10 14:20:49 -0500

    Backend Complete

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { register } from "./controllers/auth.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
+
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url); // grab file url specifically for modules (type statement in package.json)
 const __dirname = path.dirname(__filename);
@@ -39,14 +46,15 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
-// hit register route, use middleware to upload picture in assets
-// create a register controller to handle auth
-// Not in routes due to "upload" from multer which must be in index
+// Not in routes due to "upload" middleware from multer which must be in index.js
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose.set("strictQuery", false);
@@ -58,5 +66,9 @@ mongoose
 
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port : ${PORT}`));
+
+    // ADD DATA ONE TIME AND DELETE
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
